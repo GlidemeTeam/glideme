5dfeb6613ae786b7d179bd7269d79f5c22ee26e3
Krotton <krotton@gmail.com>
2015-01-18 02:15:31 +0100

    Implemented regulation.

--- src/glideme/Regulator.java
+++ src/glideme/Regulator.java
@@ -1,15 +1,168 @@
 package glideme;
 
+import glideme.fuzzy.FuzzySet;
+
+import java.util.*;
+
 /**
  * Responsible for controlling the crane using fuzzy-logical rules.
  */
 public class Regulator {
+    /**
+     * Linguistic variables describing the regulated system:
+     *  - DN, DZ, DP - distance negative/zero/positive,
+     *  - AN, AZ, AP - angle negative/zero/positive,
+     *  - VN, VZ, VP - velocity negative/zero/positive.
+     */
+    private static final FuzzySet DN = new FuzzySet(FuzzySet.MembershipType.FallingSlope, -100.0, -25.0, 0.0),
+        DZ = new FuzzySet(FuzzySet.MembershipType.Pyramidal, -25.0, 0.0, 25.0),
+        DP = new FuzzySet(FuzzySet.MembershipType.RisingSlope, 0.0, 25.0, 100.0),
+        AN = new FuzzySet(FuzzySet.MembershipType.FallingSlope, -Math.PI/2.0, -Math.PI/12.0, 0.0),
+        AZ = new FuzzySet(FuzzySet.MembershipType.Pyramidal, -Math.PI/12.0, 0.0, Math.PI/12.0),
+        AP = new FuzzySet(FuzzySet.MembershipType.RisingSlope, 0.0, Math.PI/12.0, Math.PI/2.0),
+        VN = new FuzzySet(FuzzySet.MembershipType.FallingSlope, -100.0, -25.0, 0.0),
+        VZ = new FuzzySet(FuzzySet.MembershipType.Pyramidal, -25.0, 0.0, 25.0),
+        VP = new FuzzySet(FuzzySet.MembershipType.RisingSlope, 0.0, 25.0, 100.0);
+
+    /**
+     * Calculate the maximum among the given values.
+     * (Created to compensate for Java Math's own max taking only two arguments...).
+     *
+     * @param val - first value to compare (obligatory).
+     * @param others - other values to compare (optional).
+     *
+     * @return - maximum of the doubles.
+     */
+    private static double max(final Double val, final Double... others)
+    {
+        return others.length == 0? val : Math.max(val, Collections.max(Arrays.asList(others)));
+    }
+
+    /**
+     * Calculate the minimum among the given values.
+     *
+     * @param val - first value to compare (obligatory).
+     * @param others - other values to compare (optional).
+     *
+     * @return - minimum of the doubles.
+     */
+    private static double min(final Double val, final Double... others)
+    {
+        return others.length == 0? val : Math.min(val, Collections.min(Arrays.asList(others)));
+    }
+
+    /**
+     * Calculate the sum of the given values.
+     *
+     * @param values - other values to accumulate (optional).
+     *
+     * @return - sum of the doubles.
+     */
+    private static double sum(final double... values)
+    {
+        double sum = 0;
+
+        for (final double v : values) {
+            sum += v;
+        }
+
+        return sum;
+    }
+
+    /**
+     * Return degrees of membership to each of the given fuzzy sets.
+     *
+     * @param value - the value to check membership for.
+     * @param firstSet - the first fuzzy set to check against (obligatory).
+     * @param otherSets - any other fuzzy sets to check (optional).
+     *
+     * @return - an array of doubles between 0.0 and 1.0 determining the degrees of membership to the given set
+     * (in order of occurence).
+     */
+    private static double[] fuzzify(final double value, final FuzzySet firstSet, final FuzzySet... otherSets) {
+        double[] mship = new double[1 + otherSets.length];
+
+        mship[0] = firstSet.grade(value);
+
+        int i = 0;
+        for (final FuzzySet set : otherSets) {
+            mship[++i] = set.grade(value);
+        }
+
+        return mship;
+    }
+
+    /**
+     * Perform knowledge-based reasoning and return degrees of membership of the output velocity
+     * to the three fuzzy sets (in order): VN, VZ and VP.
+     *
+     * @param distMship - an array of degrees of membership of distance to DN, DZ and DN (in order).
+     * @param angleMship - an array of degrees of membership of angle to AN, AZ and AN (in order).
+     *
+     * @return - an array of degrees of membership of velocity to VN, VZ and VP (in order).
+     */
+    private static double[] reason(final double[] distMship, final double[] angleMship) {
+        double[] velMship = {0.0, 0.0, 0.0};
+
+        // Negative velocity (VN) membership:
+        velMship[0] = max(
+                min(distMship[0], angleMship[0]), // min(DN, AN)
+                min(distMship[1], angleMship[0]), // min(DZ, AN)
+                min(distMship[2], angleMship[0]), // min(DP, AN)
+                min(distMship[2], angleMship[1])  // min(DP, AZ)
+        );
+
+        // Zero velocity (VZ) membership:
+        velMship[1] = min(distMship[1], angleMship[1]); // min(DZ, AZ)
+
+        // Positive velocity (VP) membership:
+        velMship[2] = max(
+                min(distMship[0], angleMship[1]), // min(DN, AZ)
+                min(distMship[0], angleMship[2]), // min(DN, AP)
+                min(distMship[1], angleMship[2]), // min(DZ, AP)
+                min(distMship[2], angleMship[2])  // min(DP, AP)
+        );
+
+        return velMship;
+    }
+
+    /**
+     * Find the most representative value according to the given degrees of membership of velocity sets.
+     * This is done by calculating a sum of representative members of the fuzzy sets weighted by membership degrees.
+     *
+     * @param velocityMembership - an array of three degrees of membership of the output value of the sets:
+     * VN, VZ, VP (in order).
+     *
+     * @return - a new, defuzzified velocity value.
+     */
+    private static double defuzzify(final double[] velocityMembership) {
+        // Determine the singletons and weigh them...
+        final double vnVal = VN.peakValue() * velocityMembership[0],
+                vzVal = VZ.peakValue() * velocityMembership[1],
+                vpVal = VP.peakValue() * velocityMembership[2];
+
+        // ... and find the weighted average of them.
+        return sum(vnVal, vzVal, vpVal)/sum(velocityMembership);
+    }
+
     /**
      * Uses destination, current position and angle as input. Updates the velocity.
      *
      * @param world - the world that's being updated.
      */
     public static void update(World world) {
+        World.CraneState inputState = world.getCraneState();
 
+        // Fuzzification:
+        double[] distMship = fuzzify(world.getDestination() - inputState.position, DN, DZ, DP);
+        double[] angleMship = fuzzify(inputState.angle, AN, AZ, AP);
+
+        // Reasoning:
+        double[] velMship = reason(distMship, angleMship);
+
+        // Defuzzification:
+        double newVelocity = defuzzify(velMship);
+
+        world.update(null, newVelocity, null);
     }
 }
--- src/glideme/fuzzy/FuzzySet.java
+++ src/glideme/fuzzy/FuzzySet.java
@@ -1,5 +1,7 @@
 package glideme.fuzzy;
 
+import java.lang.reflect.Member;
+
 /**
  * Represents a fuzzy set and implements basic fuzzy set operations.
  *
@@ -34,23 +36,36 @@ package glideme.fuzzy;
  */
 public class FuzzySet {
     /**
+     * Supported membership function types.
+     */
+    public enum MembershipType {
+        Pyramidal,
+        FallingSlope,
+        RisingSlope
+    }
+
+    /**
+     * Membership function type.
+     */
+    private final MembershipType membershipType;
+
+    /**
      * Characteristic points of the membership function.
      */
     private final Double start, edge, end;
 
     /**
      * Create a fuzzy set given by a piecewise-linear membership function.
-     * If all parameters are specified, the membership function will be a pyramidal function.
-     * If either start or end is null (but not both!), the function will be a *-slope function.
-     * In any other case, IllegalArgumentException will be thrown.
      *
-     * @param start - the start point of the rising slope (or null if there is none).
+     * @param membershipType - membership function type.
+     * @param start - start point of the left piece of the function (or null if it's -infinity).
      * @param edge - the coordinate of the edge.
-     * @param end - the end point of the falling slope (or null if there is none).
+     * @param end - end point of the right piece of the function (or null if it's +infinity).
      *
      * @throws IllegalArgumentException - when either edge or both start and end parameters are null.
      */
-    public FuzzySet(final Double start, final Double edge, final Double end) throws IllegalArgumentException
+    public FuzzySet(final MembershipType membershipType, final Double start, final Double edge, final Double end)
+            throws IllegalArgumentException
     {
         if (edge == null || (start == null && end == null))
         {
@@ -58,6 +73,7 @@ public class FuzzySet {
                     "FuzzySet: 'edge' and at least one of 'start' and 'end' arguments must be specified!");
         }
 
+        this.membershipType = membershipType;
         this.start = start;
         this.edge = edge;
         this.end = end;
@@ -78,21 +94,52 @@ public class FuzzySet {
         }
 
         else if (point < edge) {
-            // We are either on a constant or rising part of the function.
-
-            if (start == null) {
-                return 1.0;
+            // We are either on a constant...
+            if (membershipType == MembershipType.FallingSlope) {
+                return (start == null || point >= start)? 1.0 : 0.0;
             }
 
-            return (point - start)/(edge - start);
+            // ... or rising piece of the function (or before it).
+            return point >= start? (point - start)/(edge - start) : 0.0;
         }
 
-        // We are either on a constant or falling part of the function.
+        // We are either on a constant...
+        if (membershipType == MembershipType.RisingSlope) {
+            return (end == null || point <= end)? 1.0 : 0.0;
+        }
 
+        // ... or falling part of the function (or after it).
+        return point <= end? (end - point)/(end - edge) : 0.0;
+    }
+
+    /**
+     * Get the most representative value (singleton) of the set.
+     *
+     * @return - the most representative member value of the set.
+     */
+    public double peakValue()
+    {
+        // In case of a pyramidal function, the edge is the most representative.
+        if (membershipType == MembershipType.Pyramidal) {
+            return edge;
+        }
+        else if (membershipType == MembershipType.FallingSlope) {
+            // In case of an infinite falling slope, it's technically -infinity,
+            // but as it's not very helpful, we return the edge.
+            if (start == null) {
+                return edge;
+            }
+
+            // Otherwise (in case of a finite falling slope), it's a middle value between start and edge.
+            return (start + edge)/2.0;
+        }
+
+        // Analogically to the falling slope case, for an infinite rising slope it's an edge
+        // and for a finite one it's a mean of edge and end.
         if (end == null) {
-            return 1.0;
+            return edge;
         }
 
-        return (end - point)/(end - edge);
+        return (edge + end)/2.0;
     }
 }
